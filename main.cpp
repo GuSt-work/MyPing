5b183793c9c25c63eac25835cd324edf28807c5f
Stanislav Gushchin <GuSt-work@yandex.ru>
2026-06-11 15:58:40 +0300

    SendedPacket is map!

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <ctime>
 #include <iomanip>
 #include <iomanip>
+#include <map>
 
 #include "ip.h"
 #include "icmp.h"
@@ -15,14 +16,12 @@
 using namespace std;
 
 const USHORT MAX_PACKET = 1024; //Максимальный размер ICMP пакета
-
 const USHORT ICMP_ECHO = 8;
 
 const USHORT NUMBER_PACKETS = 4;
 const USHORT RESP_TIMEOUT = 4000;
 const USHORT REQ_TIMEOUT = 1000;
 const USHORT TTL = 255;
-
 
 USHORT CURRENT_CK_SUM = 0;
 GUID CorrentGuid;
@@ -34,6 +33,11 @@ struct SendedPacket
     chrono::steady_clock::time_point sendTime;
     GUID guid;
 };
+
+bool operator<(const GUID& a, const GUID& b)
+{
+    return memcmp(&a, &b, sizeof(GUID)) < 0;
+}
 
 // Получить временной интервал в миллисекундах
 int GetDeltaTime(chrono::steady_clock::time_point startInterval)
@@ -54,27 +58,25 @@ _Put_time<char> GetTime()
     return time;
 }
 
-// True если все пакеты приняты или таймауты истекли
-bool IsResponceTimeout(vector<SendedPacket> &packets)
+
+void IsResponceTimeout(map<GUID, SendedPacket> &packets)
 {
-    bool allTimeout = true;
-
-    for(SendedPacket &packet : packets)
+    vector<GUID> forDel;
+    for(auto packet : packets)
     {
-        if(packet.received)
-            continue;
-
-        auto deltaTime = GetDeltaTime(packet.sendTime);
+        auto deltaTime = GetDeltaTime(packet.second.sendTime);
         if(deltaTime >= RESP_TIMEOUT)
         {
-            packet.received = true;
-            cout << "Packet " << packet.seq << " timeout! at " << GetTime() << "\n";
+            //packets.erase(packet.first);
+            forDel.push_back(packet.first);
+            cout << "Packet " << packet.second.seq << " timeout! at " << GetTime() << "\n";
 
-            continue;
         }
-        allTimeout = false;
     }
-    return allTimeout;
+    for(auto g : forDel)
+    {
+        packets.erase(g);
+    }
 }
 
 // Вычислить 16-битную комплиментарную сумму
@@ -127,50 +129,19 @@ bool ValidateArgs(int argc, char **argv)
     return true;
 }
 
-void PrintHexDump(const vector<char> &data, int len, int bytes_per_line = 16) {
-    std::cout << std::hex << std::setfill('0');
-    for (int i = 0; i < len; i += bytes_per_line) {
-        // Печать смещения
-        std::cout << std::setw(4) << i << "   ";
-
-        // Печать шестнадцатеричных байтов
-        for (int j = 0; j < bytes_per_line; ++j) {
-            if (i + j < len) {
-                std::cout << std::setw(2) << (unsigned int)(unsigned char)data[i + j] << " ";
-            } else {
-                std::cout << "   "; // для выравнивания, если последняя строка короче
-            }
-        }
-
-        // // Печать ASCII представления
-        // std::cout << " ";
-        // for (int j = 0; j < bytes_per_line && i + j < len; ++j) {
-        //     char c = data[i + j];
-        //     std::cout << (isprint(c) ? c : '.');
-        // }
-        std::cout << std::endl;
-    }
-    std::cout << std::dec; // вернуть десятичный формат
-}
-
 std::string GuidToString(const GUID& guid) {
     wchar_t wbuf[64];
     if (StringFromGUID2(guid, wbuf, 64) == 0)
         return "{}";
-    // Преобразование широкой строки в узкую (подходит для ASCII-символов GUID)
+
     char buf[64];
     wcstombs(buf, wbuf, 64);
     return std::string(buf);
 }
 
-void DecodeICMP(vector<char> &data, int bufSize, vector<SendedPacket> &sendedPackets)
+void DecodeICMP(vector<char> &data, int bufSize, map<GUID, SendedPacket> &sendedPackets)
 {
     const char* buf = data.data();
-
-    cout << "\n" << "Input packet "
-         << " size=" << bufSize
-         << "\n" << endl;
-    //PrintHexDump(data, bufSize);
 
     IpHeader *ip_hdr = NULL;
     IcmpHeader *icmp_hdr = NULL;
@@ -179,8 +150,7 @@ void DecodeICMP(vector<char> &data, int bufSize, vector<SendedPacket> &sendedPac
     ip_hdr = (IpHeader*)buf;
 
     USHORT iphdrlen = ip_hdr->h_len * 4;
-    auto icmpHeadLen = sizeof(IcmpHeader);
-    auto sumsize = iphdrlen + sizeof(IcmpHeader);
+
     if(iphdrlen + sizeof(IcmpHeader) > bufSize)
     {
         cout << "input packet is small\n" << endl;
@@ -189,7 +159,6 @@ void DecodeICMP(vector<char> &data, int bufSize, vector<SendedPacket> &sendedPac
 
     icmp_hdr = (IcmpHeader*)(buf + iphdrlen);
     icmp_data = (IcmpData*)(buf + iphdrlen + sizeof(IcmpHeader));
-
 
     if(icmp_hdr->i_type != 0 || icmp_hdr->i_code != 0 )
     {
@@ -200,54 +169,84 @@ void DecodeICMP(vector<char> &data, int bufSize, vector<SendedPacket> &sendedPac
         USHORT error_ip_hdr_len = error_ip_hdr->h_len * 4;
         error_icmp_data = (IcmpData*)(buf + iphdrlen + sizeof(IcmpHeader) + 4 + error_ip_hdr_len + sizeof(IcmpHeader));
 
-        auto errorsizeIP = sizeof(error_ip_hdr);
-
-
-        for(SendedPacket &sp : sendedPackets)
+        if(iphdrlen + sizeof(IcmpHeader) + 4 + error_ip_hdr_len + sizeof(IcmpHeader) > bufSize)
         {
-            if(!sp.received && sp.guid == error_icmp_data->guid)
-            {
-                auto deltaTime = GetDeltaTime(sp.sendTime);
-                cout << "\n" << "ERROR Reply for packet "
-                     //<< icmp_hdr->i_seq
-                     << " time=" << deltaTime << "ms"
-                     << " Type:" << (USHORT)icmp_hdr->i_type
-                     << " Code:" << (USHORT)icmp_hdr->i_code
-                     << " TTL:" << (USHORT)ip_hdr->ttl
-                     << "\n"
-                     << " inputGuid " << GuidToString(error_icmp_data->guid) << "\n"
-                    // << " outputGuid" <<  outputGuid
-                     << "\n" << endl;
-
-                sp.received = true;
-            }
+            cout << "input packet is small\n" << endl;
+            return;
         }
+
+        auto find = sendedPackets.find(error_icmp_data->guid);
+        if(find != sendedPackets.end())
+        {
+            auto deltaTime = GetDeltaTime(find->second.sendTime);
+            cout << "\n" << "ERROR Reply for packet "
+                 << " time=" << deltaTime << "ms"
+                 << " Type:" << (USHORT)icmp_hdr->i_type
+                 << " Code:" << (USHORT)icmp_hdr->i_code
+                 << " TTL:" << (USHORT)ip_hdr->ttl
+                 << "\n"
+                 << "Input Guid " << GuidToString(error_icmp_data->guid) << "\n"
+                 << "\n";
+
+            sendedPackets.erase(find);
+        }
+
+        // for(SendedPacket &sp : sendedPackets)
+        // {
+        //     if(!sp.received && sp.guid == error_icmp_data->guid)
+        //     {
+        //         auto deltaTime = GetDeltaTime(sp.sendTime);
+        //         cout << "\n" << "ERROR Reply for packet "
+        //              << " time=" << deltaTime << "ms"
+        //              << " Type:" << (USHORT)icmp_hdr->i_type
+        //              << " Code:" << (USHORT)icmp_hdr->i_code
+        //              << " TTL:" << (USHORT)ip_hdr->ttl
+        //              << "\n"
+        //              << "Input Guid " << GuidToString(error_icmp_data->guid) << "\n"
+        //              << "\n" << endl;
+
+        //         sp.received = true;
+        //     }
+        // }
     }
     else
     {
-        for(SendedPacket &sp : sendedPackets)
+        auto find = sendedPackets.find(icmp_data->guid);
+        if(find != sendedPackets.end())
         {
-            if(!sp.received && sp.guid == icmp_data->guid)
-            {
-                wchar_t outputGuid[64];
-                StringFromGUID2(sp.guid, outputGuid, 64);
+            auto deltaTime = GetDeltaTime(find->second.sendTime);
+            cout << "\n" << "Reply for packet "
+                 << " time=" << deltaTime << "ms"
+                 << " Type:" << (USHORT)icmp_hdr->i_type
+                 << " Code:" << (USHORT)icmp_hdr->i_code
+                 << " TTL:" << (USHORT)ip_hdr->ttl
+                 << "\n"
+                 << "Input Guid " << GuidToString(icmp_data->guid) << "\n"
+                 << "\n";
 
-                auto deltaTime = GetDeltaTime(sp.sendTime);
-                cout << "\n" << "Reply for packet "
-                     //<< icmp_hdr->i_seq
-                     << " time=" << deltaTime << "ms"
-                     << " Type:" << (USHORT)icmp_hdr->i_type
-                     << " Code:" << (USHORT)icmp_hdr->i_code
-                     << " TTL:" << (USHORT)ip_hdr->ttl
-                     << "\n"
-                     << " inputGuid  " << GuidToString(icmp_data->guid) << "\n"
-                     //<< " outputGuid " << outputGuid
-                     << "\n" << endl;
-
-
-                sp.received = true;
-            }
+            sendedPackets.erase(find);
         }
+        // for(SendedPacket &sp : sendedPackets)
+        // {
+        //     if(!sp.received && sp.guid == icmp_data->guid)
+        //     {
+        //         wchar_t outputGuid[64];
+        //         StringFromGUID2(sp.guid, outputGuid, 64);
+
+        //         auto deltaTime = GetDeltaTime(sp.sendTime);
+        //         cout << "\n" << "Reply for packet "
+        //              << " time=" << deltaTime << "ms"
+        //              << " Type:" << (USHORT)icmp_hdr->i_type
+        //              << " Code:" << (USHORT)icmp_hdr->i_code
+        //              << " TTL:" << (USHORT)ip_hdr->ttl
+        //              << "\n"
+        //              << "Input Guid  " << GuidToString(icmp_data->guid) << "\n"
+        //              << "\n" << endl;
+
+
+        //         sp.received = true;
+        //     }
+        // }
     }
 
 }
@@ -297,7 +296,6 @@ int CreateSocket(SOCKET &sockRaw)
 int main(int argc, char *argv[])
 {
     struct sockaddr_in dest, src;
-    //char *icmp_packet = NULL, *recvbuf = NULL;
 
     bool isValidate = ValidateArgs(argc, argv);
     if(!isValidate)
@@ -310,11 +308,9 @@ int main(int argc, char *argv[])
     dest.sin_family = AF_INET;
     inet_pton(AF_INET, argv[1], &(dest.sin_addr.s_addr));
 
-    int datasize = sizeof(IcmpPacket);
-
-    //recvbuf = (char*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, MAX_PACKET);
     vector<char> recvbuf(MAX_PACKET, 0);
-    vector<SendedPacket> sendedPackets;
+    //vector<SendedPacket> sendedPackets;
+    map<GUID, SendedPacket> sendedPackets;
     auto lastSendTime = chrono::steady_clock::now();
 
     fd_set fds;
@@ -327,7 +323,7 @@ int main(int argc, char *argv[])
     int nCount = 0;
     while (1)
     {
-        if(sendedPackets.size() < NUMBER_PACKETS && GetDeltaTime(lastSendTime) >= REQ_TIMEOUT)
+        if(nCount < NUMBER_PACKETS && GetDeltaTime(lastSendTime) >= REQ_TIMEOUT)
         {
             CoCreateGuid(&CorrentGuid);
 
@@ -352,28 +348,21 @@ int main(int argc, char *argv[])
                 sp.received = false;
                 sp.sendTime = chrono::steady_clock::now();
                 sp.seq = nCount;
-                sp.guid = CorrentGuid;
-                sendedPackets.push_back(sp);
+               // sp.guid = CorrentGuid;
+                sendedPackets[CorrentGuid] = sp;
 
                 lastSendTime = chrono::steady_clock::now();
                 cout << "Packet " << nCount
                      << " is send at " << GetTime()
                      << " Size " << bwrote
                      << "\n"
-                     << "GUID " << GuidToString(CorrentGuid)
-                     << "\n" << endl;
-
-                //PrintHexDump(&icmp_pack, sizeof(IcmpPacket));
-
-                cout << "sizeof(IcmpPacket) " << sizeof(IcmpPacket) << "\n"
-                     << "sizeof(icmp_pack)" << sizeof(icmp_pack) << "\n"
-                     << "sizeof(sp.guid)" << sizeof(sp.guid)
+                     << "GUID       " << GuidToString(CorrentGuid)
                      << "\n";
 
                 ++nCount;
             }
         }
-        //continue;
+
         FD_ZERO(&fds);
         FD_SET(sockRaw, &fds);
 
@@ -394,10 +383,6 @@ int main(int argc, char *argv[])
                     DecodeICMP(recvbuf, bufSize, sendedPackets);
                 }
             }
-            else
-            {
-                cout << "NOT FDISSET" << "\n";
-            }
         }
         else if(ret != 0)
         {
@@ -405,9 +390,9 @@ int main(int argc, char *argv[])
             CleanResources(sockRaw);
         }
 
-        if(IsResponceTimeout(sendedPackets) && sendedPackets.size() == NUMBER_PACKETS)
+        IsResponceTimeout(sendedPackets);
+        if(nCount >= NUMBER_PACKETS && sendedPackets.empty())
             break;
-
 
         Sleep(100);
     }
